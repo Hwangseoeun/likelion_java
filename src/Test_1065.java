a7416dd9899f1d8f5441d8a45d8ea0e777a93760
Hwangseoeun <sunny030921@gmail.com>
2023-04-28 17:50:32 +0900

    Test(1065)

---
+++ src/Test_1065.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test_1065 {
+    //프로그램의 시작점
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        int num_1 = scanner.nextInt();
+        int num_2 = scanner.nextInt();
+        int num_3 = scanner.nextInt();
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        if(num_1%2==0){
+            System.out.println(num_1);
+        }
+        if(num_2%2==0){
+            System.out.println(num_2);
+        }
+        if(num_3%2==0){
+            System.out.println(num_3);
+        }
+    }
+}
