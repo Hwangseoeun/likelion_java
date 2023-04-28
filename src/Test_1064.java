61917287e93b9e41c8cb845153ce470380ac84fa
Hwangseoeun <sunny030921@gmail.com>
2023-04-28 17:27:19 +0900

    Test(1064)

--- src/Test_1064.java
+++ src/Test_1064.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Test {
+public class Test_1064 {
     //프로그램의 시작점
     public static void main(String[] args) {
         java.util.Scanner scanner = new Scanner(System.in);
 
         int num_1 = scanner.nextInt();
         int num_2 = scanner.nextInt();
+        int num_3 = scanner.nextInt();
 
-        if(num_1>num_2){
-            System.out.println(num_1);
-        }
-        else{
-            System.out.println(num_2);
-        }
+        int min = num_1 < num_2 ? num_1 : num_2;
+        min = min < num_3 ? min : num_3;
+        System.out.println(min);
+
     }
 
 }
