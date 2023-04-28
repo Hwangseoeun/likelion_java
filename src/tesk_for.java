ebaff7b69afba15181f1b82cd0d19a55c793f1db
Hwangseoeun <sunny030921@gmail.com>
2023-04-28 16:06:57 +0900

    tesk(for)

--- src/tesk_for.java
+++ src/tesk_for.java
@@ -1,11 +1,21 @@
 public class tesk_for {
     public static void main(String[] args) {
 
-        for(int i=1;i<=5;i++){
-            for(int j=4;j>=0;j--){
+		for(int i=1;i<=5;i++) {
+			for(int k=1;k<=5-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+        for(int i=1;i<=4;i++) {
+            for(int k=1;k<=i;k++) {
                 System.out.print(" ");
             }
-            for(int k=1;k<=5;k++){
+            for(int j=1;j<=9-2*i;j++) {
                 System.out.print("*");
             }
             System.out.println("");
