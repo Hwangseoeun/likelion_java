38637c567e7a8666c1469c15787ebc4d24e73d7e
Hwangseoeun <sunny030921@gmail.com>
2023-04-28 16:28:45 +0900

    Test(1064)

---
+++ src/Test_1064.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Test {
+    //프로그램의 시작점
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        int num_1 = scanner.nextInt();
+        int num_2 = scanner.nextInt();
+
+        if(num_1>num_2){
+            System.out.println(num_1);
+        }
+        else{
+            System.out.println(num_2);
+        }
+    }
+
+}
