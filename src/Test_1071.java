47e90f9e53e8555e1f2b3d1ab5d453bce5f18427
Hwangseoeun <sunny030921@gmail.com>
2023-04-28 18:12:55 +0900

    Test(1071)

---
+++ src/Test_1071.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test_1071 {
+    //프로그램의 시작점
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            int num = scanner.nextInt();
+            if(num==0){
+                break;
+            }
+            System.out.println(num);
+        }
+
+    }
+}
