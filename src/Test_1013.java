44c786ac4618d92c320cc23b2fe1d5713c53092c
Hwangseoeun <sunny030921@gmail.com>
2023-04-30 15:54:11 +0900

    Test(1013)

---
+++ src/Test_1013.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Test_1013 {
+    public static void main(String[] args){
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.print(a);
+        System.out.print(' ');
+        System.out.print(b);
+    }
+}
