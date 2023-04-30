33084124a8def71d8a1592f45d4c64cd70888ac9
Hwangseoeun <sunny030921@gmail.com>
2023-04-30 16:09:48 +0900

    Test(1015)

---
+++ src/Test_1015.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test_1015 {
+    public static void main(String[] args){
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        double num = scanner.nextDouble();
+
+        System.out.println(String.format("%.2f",num));
+    }
+}
