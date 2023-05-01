2a07644c813cf074c480fa48d5cae27282a0f595
Hwangseoeun <sunny030921@gmail.com>
2023-05-01 19:02:51 +0900

    Quiz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello World");
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+        char[] c = s.toCharArray();
+        for (int i = c.length - 1; i >= 0; i--) {
+            System.out.println(c[i]);
+        }
+
     }
 }
